d91eff0e176c98279cbb520ddc50dd7cc848d1c0
Riyathakur235 <riyathakur8052@gmail.com>
2025-11-24 15:59:36 +0530

    tupple

---
+++ Tuples.py
@@ -0,0 +1,34 @@
+#empty tuple
+# t=()
+ 
+# #tuple having integers
+# t=(1,2,3)
+# print(t)
+
+# # nested tuple
+# t=(1,(2,3,4),[1,'raju',34,'abc']) 
+# print(t)
+
+# t="satish",
+# type(t)
+# print(type(t))
+# print(t)
+
+#print index value
+# t=('satish','murnal','naveen','sirish')
+# print(t[1])
+# print(t[-1])
+
+# t=('satish','murnal','naveen','sirish')
+# print(t[1][2])
+
+# t=(1,2,3,4,[5,6,7])
+# # t[2]='x'
+# t[4][1]='satish'
+# print(t)
+
+# t=(1,2,4)+(3,5,6)
+# print(t)
+
+t=(("satish ")*4)
+print(t)
