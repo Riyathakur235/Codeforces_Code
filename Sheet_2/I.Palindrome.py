1f4b2c525980bb3e5133986ea4a134dca2e276a7
Riyathakur235 <riyathakur8052@gmail.com>
2025-12-14 16:13:18 +0530

    div

--- Sheet_2/I.Palindrome.py
+++ Sheet_2/I.Palindrome.py
@@ -1,4 +1,13 @@
-N = input().strip()
-reversed_N = N[::-1]
+N = int(input().strip())
+original_N=N
+reversed_N=0
+while N>0:
+    digit=N%10
+    reversed_N=reversed_N*10+digit
+    N=N//10
 print(reversed_N)
-print("YES" if N == reversed_N else "NO")
+if original_N==reversed_N:
+    print("YES")
+else:
+    print("NO")        
+    
