8b01ab0a86926031aed0e17df54119860e69514c
Riyathakur235 <riyathakur8052@gmail.com>
2026-03-16 15:04:33 +0530

    matrix

---
+++ Full DSA Sheet/Matrix.java
@@ -0,0 +1,41 @@
+//  public class Matrix{
+//     public static void main(String[] args) {
+//  ArrayList<ArrayList<Integer>> matrix=new ArrayList<>();
+//         ArrayList<Integer> row1 =new ArrayList<>();
+//         row1.add(10);
+//         row1.add(20);
+//         row1.add(30);
+//         ArrayList<Integer> row2 =new ArrayList<>();
+//         row2.add(40);
+//         row2.add(50);
+//         row2.add(60);
+//         ArrayList<Integer> row3 =new ArrayList<>();
+//         row3.add(90);
+//         row3.add(50);
+//         row3.add(80);
+//         matrix.add(row1);
+//         matrix.add(row2);
+//         matrix.add(row3);
+//         System.out.println(matrix);
+
+//         ArrayList<ArrayList<Integer>> matrix2=new ArrayList<>();
+//         ArrayList<Integer> row21 =new ArrayList<>();
+//         row21.add(14);
+//         row21.add(26);
+//         row21.add(36);
+//         ArrayList<Integer> row22 =new ArrayList<>();
+//         row22.add(48);
+//         row22.add(53);
+//         row22.add(65);
+//         ArrayList<Integer> row32 =new ArrayList<>();
+//         row32.add(90);
+//         row32.add(55);
+//         row32.add(80);
+//         matrix2.add(row21);
+//         matrix2.add(row22);
+//         matrix2.add(row32);
+//         System.out.println(matrix2);
+
+
+//     }     
+// }
--- Full DSA Sheet/NextRound.java
+++ Full DSA Sheet/NextRound.java
@@ -1,24 +1,24 @@
-# // N=int(input())
-# // toatl=0
-# // for i in range(N):
-# //  total+=N
+ // N=int(input())
+ // toatl=0
+ // for i in range(N):
+ //  total+=N
 
-public class NextRound {
-   public static void main(String[] args) {
-       Scanner sc= new Scanner(System.in);
-       int n= sc.nextInt();
-       int k =sc.nextInt();
-       int[] arr= new int[n];
-       for(int i=0;i<n;i++){
-           arr[i]=sc.nextInt();
-       }
-       int threshold=arr[k-1];
-        int count=0;
-        for (int i=0;i<n;i++){
-            if(arr[i]>=threshold && arr[i]>0){
-                count++;
-            }
-        }
-        System.out.println(count);
-   }
-}
+// public class NextRound {
+//    public static void main(String[] args) {
+//        Scanner sc= new Scanner(System.in);
+//        int n= sc.nextInt();
+//        int k =sc.nextInt();
+//        int[] arr= new int[n];
+//        for(int i=0;i<n;i++){
+//            arr[i]=sc.nextInt();
+//        }
+//        int threshold=arr[k-1];
+//         int count=0;
+//         for (int i=0;i<n;i++){
+//             if(arr[i]>=threshold && arr[i]>0){
+//                 count++;
+//             }
+//         }
+//         System.out.println(count);
+//    }
+// }
