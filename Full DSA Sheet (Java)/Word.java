cf11a49b98a3d62e2d53e5de12f1f4ba0b84ea54
Riyathakur235 <riyathakur8052@gmail.com>
2026-04-30 20:48:30 +0530

    word

---
+++ Full DSA Sheet (Java)/Word.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        
+        int upper = 0, lower = 0;
+        
+        for (char ch : s.toCharArray()) {
+            if (Character.isUpperCase(ch)) {
+                upper++;
+            } else {
+                lower++;
+            }
+        }
+        
+        if (upper > lower) {
+            System.out.println(s.toUpperCase());
+        } else {
+            System.out.println(s.toLowerCase());
+        }
+    }
+}
