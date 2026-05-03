1a6fd88dcfaf7b29be522fcceef89af2fec1dada
Riyathakur235 <riyathakur8052@gmail.com>
2026-05-03 19:27:57 +0530

    fib

---
+++ Full DSA Sheet (Java)/Fibonacci.java
@@ -0,0 +1,15 @@
+public class Fibonacci {
+    
+  // public static boolean isValid(String s){
+      // Scanner sc= new Scanner(System.in);
+      // int N=sc.nextInt();
+    //    int a=0;
+    //    int b=1;
+    //    System.out.print(a+" "+b+" ");
+    //    for(int i=3;i<N;i++){
+    //        int c=a+b;
+    //        System.out.print(c+" ");
+    //        a=b;
+    //        b=c;
+    //   }
+}
